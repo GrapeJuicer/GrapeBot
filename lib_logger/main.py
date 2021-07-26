d32ef1fe77c00cdc4214d8b5aa6e9e4ecbaef8aa
GrapeJuicer <Dev.Grape@outlook.jp>
2021-07-26 16:40:30 +0900

    [add #4] initial commit

---
+++ lib_logger/main.py
@@ -0,0 +1,62 @@
+import multiprocessing
+import sys
+
+sys.path.append("..")  # .../discordbot/lib_logger で実行されたとき
+sys.path.append(".")  # .../discordbot で実行されたとき
+
+import os
+import discord as dc
+# import lib_logger.logger as log
+import processflag.pflag as pf
+
+
+vsDbName = "vcstatus.db"
+vsTableName = "vcstatus"
+
+
+intents: dc.Intents = dc.Intents.all()
+client: dc.Client = dc.Client(intents=intents)
+
+
+# multi-process var
+activeProcessFlag: multiprocessing.Value
+flagMP = False
+
+
+# call command
+gbauth = [
+    "!gb",
+    "!grape",
+    "!grapebot"
+]
+
+
+@client.event
+async def on_ready():
+    # set flag or disp messsage
+    if flagMP:
+        activeProcessFlag.value = pf.ACTIVE
+    else:
+        print("'logger' module ready. (user: {0}, id: {1})".format(client.user.name, client.user.id))
+
+
+def launch_logger(token: str, activeFlag: multiprocessing.Value = None):
+    # set flag
+    if not activeFlag == None:
+        global activeProcessFlag
+        global flagMP
+        activeProcessFlag = activeFlag
+        activeProcessFlag.value = pf.LAUNCHING
+        flagMP = True
+    
+    client.run(token)
+    
+    # set flag
+    if not activeFlag == None:
+        activeProcessFlag.value = pf.INACTIVE
+
+
+# main ---
+if __name__ == "__main__":
+    with open("../token.txt") as f:
+        launch_logger(f.read())
